0771869e9eb2869f021569a7986eff3faaa97904
Markus Zapke-Gründemann <markus@transcode.de>
2014-08-11 20:32:31 +0200

    Replaced double quotes with single quotes

--- luftqualitaet_sachsen/measuring_stations/views.py
+++ luftqualitaet_sachsen/measuring_stations/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import TemplateView
 from django.shortcuts import render
+
 from .models import MeasuringPoint
 
 
 def overview(request):
     ctx = {'measuring_points': MeasuringPoint.objects.all()}
-    return render(request, "measuring_stations/home.html", ctx)
+    return render(request, 'measuring_stations/home.html', ctx)
