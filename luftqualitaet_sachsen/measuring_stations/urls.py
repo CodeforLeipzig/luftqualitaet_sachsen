6c127a7d455ab5ba34ae7916884fc40a9c65db3d
Markus Zapke-Gründemann <markus@transcode.de>
2014-08-11 20:32:08 +0200

    Added name for home URL

--- luftqualitaet_sachsen/measuring_stations/urls.py
+++ luftqualitaet_sachsen/measuring_stations/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
+
 from measuring_stations import views
 
+
 urlpatterns = [
-    url(r'^$', views.overview),
+    url(r'^$', views.overview, name='measuring_stations_measuringpoint_home'),
 ]
