e1abedd7c08ea3155001cc0f0b2bc64581a44ed3
Martin Feige <martin.feige@unister.de>
2015-01-05 21:18:27 +0100

    add impressum

--- luftqualitaet_sachsen/config/urls.py
+++ luftqualitaet_sachsen/config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.views.generic import TemplateView
 from django.conf.urls import include, patterns, url
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -9,5 +10,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('measuring_stations.urls'))
+    url(r'^', include('measuring_stations.urls')),
+    url(r'^impressum/', TemplateView.as_view(template_name='impressum.html'), name='impressum')
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
