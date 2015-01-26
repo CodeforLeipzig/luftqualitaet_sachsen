88ce135556152c28224346d8018152f259964e6b
Markus Zapke-Gründemann <markus@transcode.de>
2015-01-26 20:36:07 +0100

    Updated copyright notices to 2015

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Luftqualität Sachsen'
-copyright = u'2014, OK Lab Leipzig'
+copyright = u'2014 - 2015, OK Lab Leipzig'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
