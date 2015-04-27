6ca497f260db5c1be511a4e1d6d67985f1091e41
Martin Feige <martin.feige@unister.de>
2015-04-27 20:07:01 +0200

    initial add not working state

--- luftqualitaet_sachsen/tests/test_measuring_stations/test_models.py
+++ luftqualitaet_sachsen/tests/test_measuring_stations/test_models.py
@@ -1,6 +1,7 @@
 from measuring_stations.models import MeasuringPoint
 
-
 def test_get_active_values():
+
     mp = MeasuringPoint(so2=True, o3=True)
+    mp.save()
     assert mp.get_active_values() == ('so2', 'o3')
