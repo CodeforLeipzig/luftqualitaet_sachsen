a5379cbe5b77e5ae047374594b94a845cb52d3b4
Martin Feige <martin.feige@unister.de>
2015-06-23 07:56:29 +0200

    pep8 clean to long line

--- luftqualitaet_sachsen/measuring_stations/urls.py
+++ luftqualitaet_sachsen/measuring_stations/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from measuring_stations import views
 
@@ -10,4 +11,6 @@ urlpatterns = [
         name='measuring_stations_measuringpoint_detail'),
     url(r'^station/(?P<slug>[-\w]+)\.csv$', views.MeasuringPointCSVView.as_view(),
         name='measuring_stations_measuringpoint_csv'),
+    url(r'^erklaerung/$', TemplateView.as_view(
+        template_name='measuring_stations/explaination.html'), name='explaination')
 ]
