c473dd500594a449171b8584c0520e4e8b2a2fbe
Eric Kelm <eric.kelm@web.de>
2015-11-09 21:59:20 +0100

    #21 add stations for all pollutants

--- luftqualitaet_sachsen/measuring_stations/management/commands/fetchStations.py
+++ luftqualitaet_sachsen/measuring_stations/management/commands/fetchStations.py
@@ -5,12 +5,14 @@ import csv
 import gevent.monkey
 import requests
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from optparse import make_option
 from HTMLParser import HTMLParser
 
 from srtm import get_data
 from geoposition.fields import Geoposition
 from geopy.geocoders import Nominatim
+from gevent.pool import Pool
 from decimal import Decimal
 from progressbar import ProgressBar, Percentage, Bar
 
@@ -36,14 +38,48 @@ class Command(BaseCommand):
 
     URL = "http://www.umweltbundesamt.de/luftdaten/stations/locations?date=20150101" #dummy date param
 
-    STATIONEN = {}
+    POLLUTANTS = {
+        "NO2": "NO2",
+        "O3": "O3",
+        "PM10": "PM1",
+        "SO2": "SO2",
+        "CO": "CO",
+    }
+
+    AVERAGE_POLLUTANT_MAP = {
+        "NO2": "1SMW",
+        "O3": "1SMW",
+        "PM10": "1TMW",
+        "SO2": "1SMW",
+        "CO": "8SMW",
+    }
+
+    POLLUTANTS_FIELD_MAP = {
+        "NO2": "no2",
+        "O3": "o3",
+        "PM10": "pm10",
+        "SO2": "so2",
+        "CO": "co",
+    }
 
     def handle(self, *args, **options):
         self.options = options
         gevent.monkey.patch_socket()
 
-        params = {}
         self.s = requests.Session()
+
+        #pollutantPool = Pool(len(self.POLLUTANTS))
+        pollutantPool = Pool(1)
+
+        for pollutant in self.POLLUTANTS:
+            pollutantPool.spawn(self.fetchStations, pollutant)
+
+        pollutantPool.join()
+
+    def fetchStations(self, pollutant):
+        params = {}
+        params['pollutant'] = self.POLLUTANTS[pollutant]
+        params['data_type'] = self.AVERAGE_POLLUTANT_MAP[pollutant]
 
         response = self.s.post(self.URL, params, timeout=self.options['timeout'])
         response.raise_for_status()
@@ -51,32 +87,37 @@ class Command(BaseCommand):
         lines = response.content.splitlines()
         reader = csv.DictReader(lines, delimiter='\t')
         geolocator = Nominatim()
-        pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=len(lines)).start()
+        pbar = ProgressBar(widgets=[pollutant + ': ', Percentage(), Bar()], maxval=len(lines)).start()
         i = 0
 
         for row in reader:
-            address = geolocator.reverse((row['lat'], row['lon'])).raw['address']
+            defaults = {}
+            defaults['name'] = self.getStationName(row)
+            defaults['position'] = self.getPosition(row)
+            defaults['amsl'] = self.getElevation(row)
+            defaults['form_id'] = self.getFormId(row)
+            defaults['slug'] = self.getSlug(row)
 
-            name = self.getStationName(row)
-            position = self.getPosition(row)
-            location = self.getLocationName(address)
-            city = self.getCityName(address)
-            amsl = self.getElevation(row)
-            form_id = self.getFormId(row)
-            slug = self.getSlug(row)
+            with transaction.atomic():
+                entry = MeasuringPoint.objects.select_for_update().filter(name=defaults['name']).last()
 
-            MeasuringPoint.objects.update_or_create(
-                name=name,
-                defaults={
-                    'name': name,
-                    'position': position,
-                    'location': location,
-                    'city': city,
-                    'amsl': amsl,
-                    'form_id': form_id,
-                    'slug': slug
-                }
-                )
+                if (entry == None):
+                    entries = MeasuringPoint.objects.select_for_update().filter(slug=defaults['slug']).last()
+
+                    if (entry == None):
+                        address = geolocator.reverse((row['lat'], row['lon'])).raw['address']
+                        defaults['location'] = self.getLocationName(address)
+                        defaults['city'] = self.getCityName(address)
+                        defaults[self.POLLUTANTS_FIELD_MAP[pollutant]] = True
+                        MeasuringPoint.objects.create(**defaults)
+                    else:
+                        entry[self.POLLUTANTS_FIELD_MAP[pollutant]] = True
+                        entry.save()
+
+                else:
+                    setattr(entry, self.POLLUTANTS_FIELD_MAP[pollutant], True)
+                    entry.save()
+
             pbar.update(i)
             i = i + 1
 
@@ -103,8 +144,10 @@ class Command(BaseCommand):
             location = address['path']
         elif 'suburb' in address:
             location = address['suburb']
-        else:
+        elif 'farmyard' in address:
             location = address['farmyard']
+        else:
+            location = address['footway']
 
         return location
 
